97054ddae04026e42ad0914700069eb895748e53
Juan <jc-mtz55@hotmail.com>
2022-02-17 16:40:55 -0700

    added cartIcon and cartDropdown with its reducer

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -59,7 +59,7 @@ class SignIn extends React.Component {
           />
 
           <div className='buttons'>
-          <CustomButton type="submit" handleClick={this.handleSubmit}>
+          <CustomButton type="submit">
             Sign in
           </CustomButton>
           <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
